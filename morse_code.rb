a8bba31083673f796073480a8672021a6d8a5eae
Cédric Kossi <lionel.kossi@gmail.com>
2022-06-08 17:45:27 +0000

    Fix Linters

--- morse_code.rb
+++ morse_code.rb
@@ -1,60 +1,62 @@
-ALPHA_TO_MORSE = {
-  "a" => ".-",
-  "b" => "-...",
-  "c" => "-.-.",
-  "d" => "-..",
-  "e" => ".",
-  "f" => "..-.",
-  "g" => "--.",
-  "h" => "....",
-  "i" => "..",
-  "j" => ".---",
-  "k" => "-.-",
-  "l" => ".-..",
-  "m" => "--",
-  "n" => "-.",
-  "o" => "---",
-  "p" => ".--.",
-  "q" => "--.-",
-  "r" => ".-.",
-  "s" => "...",
-  "t" => "-",
-  "u" => "..-",
-  "v" => "...-",
-  "w" => ".--",
-  "x" => "-..-",
-  "y" => "-.--",
-  "z" => "--..",
-  "1" => ".----",
-  "2" => "..---",
-  "3" => "...--",
-  "4" => "....-",
-  "5" => ".....",
-  "6" => "-....",
-  "7" => "--...",
-  "8" => "---..",
-  "9" => "----.",
-  "0" => "-----",
-  " " => "/" 
-  }
+@alpha_to_morse = {
+  'a' => '.-',
+  'b' => '-...',
+  'c' => '-.-.',
+  'd' => '-..',
+  'e' => '.',
+  'f' => '..-.',
+  'g' => '--.',
+  'h' => '....',
+  'i' => '..',
+  'j' => '.---',
+  'k' => '-.-',
+  'l' => '.-..',
+  'm' => '--',
+  'n' => '-.',
+  'o' => '---',
+  'p' => '.--.',
+  'q' => '--.-',
+  'r' => '.-.',
+  's' => '...',
+  't' => '-',
+  'u' => '..-',
+  'v' => '...-',
+  'w' => '.--',
+  'x' => '-..-',
+  'y' => '-.--',
+  'z' => '--..',
+  '1' => '.----',
+  '2' => '..---',
+  '3' => '...--',
+  '4' => '....-',
+  '5' => '.....',
+  '6' => '-....',
+  '7' => '--...',
+  '8' => '---..',
+  '9' => '----.',
+  '0' => '-----',
+  ' ' => '/'
+}
 
-  MORSE_TO_ALPHA = ALPHA_TO_MORSE.invert
+def morse_to_alpha_map
+  @alpha_to_morse.invert
+end
 
-  def decode_char(str)
-    MORSE_TO_ALPHA[str].upcase
-  end
+def decode_char(str)
+  morse_to_alpha_map[str].upcase
+end
 
-  def decode_word(str)
-    str.split(" ").map { |char| decode_char(char) }.join
-  end
+def decode_word(str)
+  str.split.map { |char| decode_char(char) }.join
+end
 
-  def morse_to_alpha(str)
-    str.split("   ").map { |word| decode_word(word)}.join(" ")
-  end
+def morse_to_alpha(str)
+  str.split('   ').map { |word| decode_word(word) }.join(' ')
+end
 
-  # Bonus
-  def alpha_to_morse(str)
-    str.split("").map { |letter| ALPHA_TO_MORSE[letter] }.join(" ")
-  end
+# Bonus
+# def alpha_to_morse(str)
+#   str.char.map { |letter| alpha_to_morse[letter] }.join(' ')
+# end
 
-  print (morse_to_alpha(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...") + "\n")
+printf(morse_to_alpha('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
